954dcd6b0f33fd3d603c790b73288e2dc99518de
egaska <48637803+egaska@users.noreply.github.com>
2020-05-08 12:09:02 -0500

    Revert "added flowjs and styled page"

--- script.js
+++ script.js
@@ -10,14 +10,6 @@ var recipe = {
     ingr: []
 }
 
-$('body').flowtype({
-    minimum   : 500,
-    maximum   : 1200,
-    minFont   : 12,
-    maxFont   : 18,
-    fontRatio : 30
-   });
-
 //Function to run upon page load
 loadInitialHistory();
 prependHistoryElement()
